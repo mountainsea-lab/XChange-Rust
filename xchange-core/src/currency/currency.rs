f8a94ea49430e71e5b834ddc62b290d3539e3b01
www1 <w1999wtw3537@sina.com>
2025-11-13 16:25:32 +0800

    Arc<str>->String

--- xchange-core/src/currency/currency.rs
+++ xchange-core/src/currency/currency.rs
@@ -1,1 +1,16 @@
+use crate::currency::CurrencyAttributes;
+use serde::{Deserialize, Serialize};
 
+/**
+ * A Currency class roughly modeled after {@link java.util.Currency}. Each object retains the code
+ * it was acquired with -- so {@link #getInstance}("BTC").{@link #getCurrencyCode}() will always be
+ * "BTC", even though the proposed ISO 4217 code is "XBT"
+ */
+#[derive(Debug, Clone, Hash, Serialize, Deserialize)]
+pub struct Currency {
+    /// 货币代码(如 "BTC", "USD", "ETH")
+    code: String,
+
+    /// 货币属性
+    attributes: CurrencyAttributes,
+}
