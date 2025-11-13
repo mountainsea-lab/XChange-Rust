de0fa229c6581d306d1616775e2cc889d82ea52f
mountainsea-lab <w1999wtw3537@gmail.com>
2025-11-13 21:25:29 +0800

    add new,from_symbols

--- xchange-core/src/currency/currency_pair.rs
+++ xchange-core/src/currency/currency_pair.rs
@@ -15,3 +15,32 @@ pub struct CurrencyPair {
     pub base: Currency,
     pub counter: Currency,
 }
+
+impl CurrencyPair {
+    ///    Full constructor In general the CurrencyPair.base is what you're wanting to buy/sell. The
+    ///    CurrencyPair.counter is what currency you want to use to pay/receive for your purchase/sale.
+    ///
+    ///    @param base The base currency is what you're wanting to buy/sell
+    ///    @param counter The counter currency is what currency you want to use to pay/receive for your
+    ///       purchase/sale.
+    pub fn new(base: Currency, counter: Currency) -> Self {
+        Self { base, counter }
+    }
+
+    ///  String constructor In general the CurrencyPair.base is what you're wanting to buy/sell. The
+    ///    CurrencyPair.counter is what currency you want to use to pay/receive for your purchase/sale.
+    ///
+    ///    @param baseSymbol The base symbol is what you're wanting to buy/sell
+    ///    @param counterSymbol The counter symbol is what currency you want to use to pay/receive for
+    ///       your purchase/sale.
+    pub fn from_symbols(base_symbol: &str, counter_symbol: &str) -> Self {
+        let base = Currency::instance(base_symbol);
+        let counter = Currency::instance(counter_symbol);
+        Self::new(base, counter)
+    }
+
+    /// 返回 "EUR/USD" 等格式
+    pub fn symbol(&self) -> String {
+        format!("{}/{}", self.base.code, self.counter.code)
+    }
+}
