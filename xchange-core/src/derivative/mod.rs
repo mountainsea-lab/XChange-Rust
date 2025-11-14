7cbd16c924b355e1091315493ebee0fddec03e56
mountainsea-lab <w1999wtw3537@gmail.com>
2025-11-14 23:10:12 +0800

    Ord,PartialOrd for OptionType

--- xchange-core/src/derivative/mod.rs
+++ xchange-core/src/derivative/mod.rs
@@ -2,6 +2,7 @@ pub mod futures_contract;
 pub mod options_contract;
 
 use crate::currency::currency_pair::CurrencyPair;
+use std::cmp::Ordering;
 use std::fmt;
 use std::str::FromStr;
 
@@ -13,6 +14,23 @@ pub trait Derivative {
 pub enum OptionType {
     Call,
     Put,
+}
+
+impl PartialOrd for OptionType {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for OptionType {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (OptionType::Call, OptionType::Call) => Ordering::Equal,
+            (OptionType::Call, OptionType::Put) => Ordering::Less,
+            (OptionType::Put, OptionType::Call) => Ordering::Greater,
+            (OptionType::Put, OptionType::Put) => Ordering::Equal,
+        }
+    }
 }
 
 impl FromStr for OptionType {
