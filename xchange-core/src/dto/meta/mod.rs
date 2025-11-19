779260adca2ce474c55c91d9d861aaa54033ab45
mountainsea-lab <w1999wtw3537@gmail.com>
2025-11-19 21:29:45 +0800

    pub enum WalletHealth

--- xchange-core/src/dto/meta/mod.rs
+++ xchange-core/src/dto/meta/mod.rs
@@ -1,1 +1,18 @@
+/// Represents the health status of a wallet on the exchange.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum WalletHealth {
+    /// You can deposit and withdraw funds from the exchange
+    Online,
 
+    /// Deposits are disabled
+    DepositsDisabled,
+
+    /// Withdrawals are disabled
+    WithdrawalsDisabled,
+
+    /// You cannot deposit nor withdraw funds from the exchange
+    Offline,
+
+    /// The exchange does not inform us about the health of this wallet
+    Unknown,
+}
