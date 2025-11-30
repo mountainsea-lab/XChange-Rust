3f2065e65ecb23fabf170fa0251c7868ab11d273
mountainsea-lab <w1999wtw3537@gmail.com>
2025-11-30 16:19:27 +0800

    define pub struct BinanceBaseService

--- xchange-binance/src/service/binance_base_service.rs
+++ xchange-binance/src/service/binance_base_service.rs
@@ -1,1 +1,14 @@
+use crate::client::binance_authed::BinanceAuthedClient;
+use crate::client::binance_futures::BinanceFuturesAuthedClient;
+use std::sync::Arc;
+use xchange_core::rescu::params_digest::ParamsDigest;
 
+pub struct BinanceBaseService {
+    pub api_key: String,
+
+    pub spot: Arc<BinanceAuthedClient>,
+    pub futures: Option<Arc<BinanceFuturesAuthedClient>>,
+    pub inverse_futures: Option<Arc<BinanceFuturesAuthedClient>>,
+
+    pub digest: Arc<dyn ParamsDigest + Send + Sync>,
+}
